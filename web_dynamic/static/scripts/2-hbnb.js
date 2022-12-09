a0068d488a67f4a9a2feb410bed29a5ed0c0be6e
bbrown585 <brandon_scott585@yahoo.com>
2022-12-09 12:54:02 -0800

    2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    const amenities = [];
+    $('li :checkbox').change(function() {
+      if (this.checked) {
+        amenities.push($(this).attr('data-name'));
+      } else {
+        amenities.pop();
+      }
+      $('.amenities h4').html(amenities.join(', '));
+    });
+  });
+  
+$.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    type: 'GET',
+    dataType: 'json'
+  })
+    .done(function (json) {
+      const resp = json.results;
+      console.log(resp);
+    }
+    );
