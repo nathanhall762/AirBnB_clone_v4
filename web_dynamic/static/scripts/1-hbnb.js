a1a1a635e85444c91afa27f74f8a2f800db661aa
nathanhall762 <4943@holbertonstudents.com>
2022-12-09 13:17:03 -0600

    fill and show dict

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,9 +4,12 @@ $(document).ready(function () {
   $('input[type=checkbox]').click(function () {
     if ($(this).is(':checked')) {
       console.log('checked');
-      $('div.amenities h4').text('test');
+	  amenityDict[$(this).data('id')] = $(this).data('name');
+      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
     } else {
       console.log('unchecked');
+	  delete amenityDict[$(this).data('id')]
+      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
     }
   });
 });
