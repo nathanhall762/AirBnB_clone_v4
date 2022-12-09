306698c094b2cbf66eec2926221229fb21e04841
nathanhall762 <4943@holbertonstudents.com>
2022-12-09 15:12:35 -0600

    test ajax get request

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    const amenities = [];
-    $('li :checkbox').change(function() {
-      if (this.checked) {
-        amenities.push($(this).attr('data-name'));
-      } else {
-        amenities.pop();
-      }
-      $('.amenities h4').html(amenities.join(', '));
-    });
+$(document).ready(function () {
+  const amenities = [];
+  $('li :checkbox').change(function () {
+    if (this.checked) {
+      amenities.push($(this).attr('data-name'));
+    } else {
+      amenities.pop();
+    }
+    $('.amenities h4').html(amenities.join(', '));
   });
+});
