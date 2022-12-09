e1b3706ecf9c19b734c3fecc9ab86c354ec467ad
nathanhall762 <4943@holbertonstudents.com>
2022-12-09 13:23:12 -0600

    dict to list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,14 @@
 const $ = window.$;
 $(document).ready(function () {
-  const amenityDict = {};
+  const amenityList = [];
   $('input[type=checkbox]').click(function () {
     if ($(this).is(':checked')) {
       console.log('checked');
-	  amenityDict[$(this).data('id')] = $(this).data('name');
-      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
+	  amenityList.push($(this).attr('data-name'));
     } else {
       console.log('unchecked');
-	  delete amenityDict[$(this).data('id')]
-      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
+      amenityList.pop();
     }
+    $('div.amenities h4').text(amenityList).join(', ');
   });
 });
