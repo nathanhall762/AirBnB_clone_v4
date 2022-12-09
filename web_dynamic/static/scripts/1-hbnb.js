44e891b0926d269fd07a73a33d76473538cc4fbe
nathanhall762 <4943@holbertonstudents.com>
2022-12-09 12:51:53 -0600

    fix html string

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,6 +2,11 @@ const $ = window.$;
 $(document).ready(function () {
   const amenityDict = {};
   $('input[type=checkbox]').click(function () {
-    if ($(this).is(':checked')) { console.log('checked'); $('div.h4').html(<p>test</p>); } else { console.log('unchecked'); }
+    if ($(this).is(':checked')) {
+      console.log('checked');
+      $('div.h4').html('<p>test</p>');
+    } else {
+      console.log('unchecked');
+    }
   });
 });
