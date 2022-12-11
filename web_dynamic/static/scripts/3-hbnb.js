63a77af5036ce08753c17379d8ce4a715af74d8d
nathanhall762 <4943@holbertonstudents.com>
2022-12-11 15:10:59 -0600

    post api test

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  const amenities = [];
+  $('li :checkbox').change(function () {
+    if (this.checked) {
+      amenities.push($(this).attr('data-name'));
+    } else {
+      amenities.pop();
+    }
+    $('.amenities h4').html(amenities.join(', '));
+  });
+  getStatus('http://c66a74704532.93090521.hbtn-cod.io:5001/api/v1/status/');
+});
+
+function getStatus (url) {
+  $.get(url, function (json) {
+    if (json.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+}
+
+$.post('http://0.0.0.0:5001/api/v1/places_search/',
+  {},
+  function (data, status) {
+    alert('Data: ' + data + '\nStatus: ' + status);
+  });
