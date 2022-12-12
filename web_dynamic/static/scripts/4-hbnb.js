361702140c1a170c553ecda63cd97abe41583476
nathanhall762 <4943@holbertonstudents.com>
2022-12-12 11:13:41 -0600

    included button functionality

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,47 +1,84 @@
 $(document).ready(function () {
-    const amenities = [];
-    $('li :checkbox').change(function () {
-      if (this.checked) {
-        amenities.push($(this).attr('data-name'));
-      } else {
-        amenities.pop();
+  const amenities = [];
+  $('li :checkbox').change(function () {
+    if (this.checked) {
+      amenities.push($(this).attr('data-name'));
+    } else {
+      amenities.pop();
+    }
+    $('.amenities h4').html(amenities.join(', '));
+  });
+
+  const placesUri = 'http://c66a74704532.93090521.hbtn-cod.io:5001/api/v1/places_search/';
+
+  $.ajax({
+    url: placesUri,
+    type: 'POST',
+    contentType: 'application/json',
+    data: '{}',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        parsePlace(data[i]);
       }
-      $('.amenities h4').html(amenities.join(', '));
-    });
-  
-    const placesUri = 'http://c66a74704532.93090521.hbtn-cod.io:5001/api/v1/places_search/';
-  
+    }
+  });
+
+  $('button').click(function () {
     $.ajax({
       url: placesUri,
       type: 'POST',
       contentType: 'application/json',
       data: '{}',
+      amenities: amenities,
       success: function (data) {
-        console.log
         for (let i = 0; i < data.length; i++) {
-          buildPlaceArticle(data[i]);
+          parsePlace(data[i]);
         }
       }
     });
-
-    $('button').click(function() {
-      alert( "Handler for .click() called." );
-    });
-  
-    getStatus('http://c66a74704532.93090521.hbtn-cod.io:5001/api/v1/status/');
   });
-  
-  function getStatus (url) {
-    $.get(url, function (json) {
-      if (json.status === 'OK') {
-        $('div#api_status').addClass('available');
-      } else {
-        $('div#api_status').removeClass('available');
-      }
-    });
-  }
-  
-  function buildPlaceArticle (data) {
-    console.log(data);
-  }
-  
+
+  getStatus('http://c66a74704532.93090521.hbtn-cod.io:5001/api/v1/status/');
+});
+
+function getStatus (url) {
+  $.get(url, function (json) {
+    if (json.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+}
+
+function parsePlace (place) {
+  console.log('into parse');
+  return placeBuilder(
+    place.name,
+    place.price_by_night,
+    place.max_guest,
+    place.number_rooms,
+    place.number_bathrooms,
+    place.description
+  );
+}
+
+function placeBuilder (title, priceByNight, maxGuest, numberRooms, numberBathrooms, description) {
+  console.log('into builder');
+  return `<article>
+      <div class="title_box">
+        <h2>${title}</h2>
+        <div class="price_by_night">$${priceByNight}</div>
+      </div>
+      <div class="information">
+        <div class="max_guest">${maxGuest} Guest(s)</div>
+              <div class="number_rooms">${numberRooms} Bedroom(s)</div>
+              <div class="number_bathrooms">${numberBathrooms} Bathroom(s)</div>
+      </div>
+      <div class="user">
+            </div>
+            <div class="description">
+        ${description}
+            </div>
+    </article>`;
+}
